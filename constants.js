ea4b056b4e66d16b2975ed747ace116c0c5e841a
loclamor <loclamor@gmail.com>
2019-12-12 16:36:22 +0100

    Add missing file

---
+++ constants.js
@@ -0,0 +1,80 @@
+var OgameConstants = {
+	resourceTypes: {
+		metal_box : 'M',
+		crystal_box : 'C',
+		deuterium_box : 'D',
+		energy_box : 'E',
+		darkmatter_box : 'A'
+	},
+	resources: {
+		metal: 1,
+		cristal: 2,
+		deut: 3,
+		ces: 4,
+		cef: 12,
+		sat: 212,
+		foreuse: 217
+	},
+	research: {
+		energy: 113,
+		laser: 120,
+		ion: 121,
+		hyperspace: 114,
+		plasma: 122,
+		combustionDrive: 115,
+		impulseDrive: 117,
+		hyperspaceDrive: 118,
+		espionage: 106,
+		computer: 108,
+		astrophysics: 124,
+		researchNetwork: 123,
+		graviton: 199,
+		weapons: 109,
+		shielding: 110,
+		armor: 111
+	},
+	buildings: {
+		resources: {
+			metalMine: 1,
+			crystalMine: 2,
+			deuteriumSynthesizer: 3,
+			solarPlant: 4,
+			fusionPlant: 12,
+			solarSatellite: 112,
+			resbuggy: 217,
+			metalStorage: 22,
+			crystalStorage: 23,
+			deuteriumStorage: 24
+		},
+		installations: {
+			roboticsFactory: 14,
+			shipyard: 21,
+			researchLaboratory: 31,
+			allianceDepot: 34,
+			missileSilo: 44,
+			naniteFactory: 15,
+			terraformer: 33,
+			repairDock: 36
+		}
+	},
+	fleet: {
+		fighterLight: 204,
+		fighterHeavy: 205,
+		cruiser: 206,
+		battleship: 207,
+		interceptor: 215,
+		bomber: 211,
+		destroyer: 213,
+		deathstar: 214,
+		reaper: 218,
+		explorer: 219,
+		transporterSmall: 202,
+		transporterLarge: 203,
+		colonyShip: 208,
+		recycler: 209,
+		espionageProbe: 210,
+		solarSatellite: 212,
+		resbuggy: 217
+	}
+	
+}
