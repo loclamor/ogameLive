1164bf4c4d91cb463f03884a0f1a4110a9cc9e2a
loclamor <favier.loic.31@gmail.com>
2019-12-14 16:08:46 +0100

    [WIP] Flying fleet parsing

--- utilities.js
+++ utilities.js
@@ -578,5 +578,16 @@ function better_fleet_display() {
 	});
 }
 
+function parseFlyingFleet() {
+	//Attach a mutation observer onto the eventbox 
+	// @Todo !
+	
+	// open eventBox...
+	jQuery('#js_eventDetailsClosed').click();
+	// ...and close it imediatly !
+	jQuery('#js_eventDetailsClosed').click();
+	// event box is now loading and will be detected by mutationObserver :)
+}
+
 
 	
